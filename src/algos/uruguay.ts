04d6ec91fb9d3f799105fae35fb917503bf98396
djpeluca <adrianbs@gmail.com>
2024-11-28 01:39:27 -0300

    list members uruguay

--- src/algos/uruguay.ts
+++ src/algos/uruguay.ts
@@ -2,10 +2,15 @@ import { QueryParams } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import { AppContext } from '../config'
 import { AlgoManager } from '../addn/algoManager'
 import dotenv from 'dotenv'
+import getListMembers from '../addn/getListMembers'
+import setListMembers from '../addn/setListMembers'
+import getPostsForUser from '../addn/getPostsForUser'
+import resoveDIDToHandle from '../addn/resolveDIDToHandle'
 import { Post } from '../db/schema'
 import dbClient from '../db/dbClient'
 import getUserDetails from '../addn/getUserDetails'
 import { AppBskyGraphDefs } from '@atproto/api'
+import getUserLists from '../addn/getUserLists'
 
 dotenv.config()
 
@@ -37,6 +42,15 @@ export const handler = async (ctx: AppContext, params: QueryParams) => {
 
 export class manager extends AlgoManager {
   public name: string = shortname
+  public authorList: string[]
+  public author_collection = 'list_members'
+
+  public async start() {
+    this.authorList = await dbClient.getDistinctFromCollection(
+      this.author_collection,
+      'did',
+    )
+  }
 
   public matchTerms: string[] = [
     '🇺🇾',
@@ -156,28 +170,124 @@ export class manager extends AlgoManager {
     //
   ]
 
-  public async fetchListMembers(uri: string) {
-    let cursor: string | undefined
-    let members: AppBskyGraphDefs.ListItemView[] = []
-
-    do {
-      const res = await this.agent.app.bsky.graph.getList({
-        list: uri,
-        limit: 150,
-        cursor,
-      })
-      cursor = res.data.cursor
-      members = members.concat(res.data.items)
-    } while (cursor)
-
-    this.matchUsers = members.map(member => member.subject.did)
-  }
-
   public async periodicTask() {
+    dotenv.config()
+
     await this.db.removeTagFromOldPosts(
       this.name,
       new Date().getTime() - 7 * 24 * 60 * 60 * 1000,
     )
+
+    if (this.authorList === undefined) {
+      console.log(`${this.name}: Initialising authors from database`)
+      await this.start()
+    }
+
+    const lists: string[] = `${process.env.URUGUAY_LISTS}`.split('|')
+
+    if (process.env.SCIENCE_RECURSE === 'true') {
+      const list_owners: string[] = []
+
+      for (const list of lists) {
+        const members = await getListMembers(list, this.agent)
+        members.forEach((member) => {
+          if (!list_owners.includes(member)) list_owners.push(member)
+        })
+      }
+      for (const owner of list_owners) {
+        const owner_lists = await getUserLists(owner, this.agent)
+        const owner_name = await resoveDIDToHandle(owner, this.agent)
+        owner_lists.forEach((list) => {
+          if (list.name.includes(`${process.env.SCIENCE_SYMBOL}`)) {
+            console.log(
+              `${this.name}: Adding ${list.name} (${owner_name}) to lists`,
+            )
+            lists.push(list.atURL)
+          }
+        })
+      }
+    }
+
+    console.log(`${this.name}: Watching ${lists.length} lists`)
+
+    let list_members: string[] = []
+
+    for (let i = 0; i < lists.length; i++) {
+      const members = await getListMembers(lists[i], this.agent)
+      members.forEach((member) => {
+        if (!list_members.includes(member)) list_members.push(member)
+      })
+    }
+
+    if (process.env.BLOCKLIST) {
+      const blocked_members: string[] = await getListMembers(
+        process.env.BLOCKLIST,
+        this.agent,
+      )
+      list_members = list_members.filter((member) => {
+        return !blocked_members.includes(member)
+      })
+    }
+
+    const db_authors = await dbClient.getDistinctFromCollection(
+      this.author_collection,
+      'did',
+    )
+
+    const new_authors = list_members.filter((member) => {
+      return !db_authors.includes(member)
+    })
+    const del_authors = db_authors.filter((member) => {
+      return !list_members.includes(member)
+    })
+
+    console.log(
+      `${this.name}: Watching ${db_authors.length} + ${new_authors.length} - ${del_authors.length} = ${list_members.length} authors`,
+    )
+
+    this.authorList = [...list_members]
+
+    await dbClient.removeTagFromPostsForAuthor(this.name, del_authors)
+
+    for (let i = 0; i < new_authors.length; i++) {
+      process.stdout.write(`${this.name}: ${i + 1} of ${new_authors.length}: `)
+      const all_posts = await getPostsForUser(new_authors[i], this.agent)
+
+      const posts: Post[] = []
+
+      for (let i = 0; i < all_posts.length; i++) {
+        if ((await this.filter_post(all_posts[i])) == true) {
+          posts.push(all_posts[i])
+        }
+      }
+
+      posts.forEach(async (post) => {
+        const existing = await this.db.getPostForURI(post.uri)
+        if (existing === null) {
+          post.algoTags = [this.name]
+          await this.db.replaceOneURI('post', post.uri, post)
+        } else {
+          const tags = [...new Set([...existing.algoTags, this.name])]
+          post.algoTags = tags
+          await this.db.replaceOneURI('post', post.uri, post)
+        }
+      })
+
+      await this.db.replaceOneDID(this.author_collection, new_authors[i], {
+        did: new_authors[i],
+      })
+    }
+
+    del_authors.forEach(async (author) => {
+      if (this.agent !== null)
+        console.log(
+          `${this.name}: Removing ${await resoveDIDToHandle(
+            author,
+            this.agent,
+          )}`,
+        )
+      await this.db.deleteManyDID(this.author_collection, [author])
+    })
   }
 
   public async filter_post(post: Post): Promise<Boolean> {
@@ -241,6 +351,10 @@ export class manager extends AlgoManager {
         match = true;
       }
     });
+
+    if (this.authorList.includes(post.author)) {
+      match = true;
+    }
 
     return match
   }
